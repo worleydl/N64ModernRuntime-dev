0a18b8a0577d8a4577934a5fec017c6a3d71ae88
= <danworley@gmail.com>
2025-12-18 12:28:11 -0500

    uwp: Hack around multi def of recomp funcs

--- librecomp/src/math_routines.cpp
+++ librecomp/src/math_routines.cpp
@@ -4,6 +4,8 @@
 // TODO remove these by implementing the necessary instructions and control flow handling in the recompiler.
 // This has already been partially completed.
 
+// DLW: Already defined in my setup
+#if 0
 extern "C" void __udivdi3_recomp(uint8_t * rdram, recomp_context * ctx) {
     uint64_t a = (ctx->r4 << 32) | ((ctx->r5 << 0) & 0xFFFFFFFFu);
     uint64_t b = (ctx->r6 << 32) | ((ctx->r7 << 0) & 0xFFFFFFFFu);
@@ -30,6 +32,7 @@ extern "C" void __umoddi3_recomp(uint8_t * rdram, recomp_context * ctx) {
     ctx->r2 = (int32_t)(ret >> 32);
     ctx->r3 = (int32_t)(ret >> 0);
 }
+#endif
 
 extern "C" void __ull_div_recomp(uint8_t * rdram, recomp_context * ctx) {
     uint64_t a = (ctx->r4 << 32) | ((ctx->r5 << 0) & 0xFFFFFFFFu);
